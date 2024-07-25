b4faefdbab00e18c9edc06282da56ec7f2f89f15
Nitin Budhlakoti <113322720+Codebreaker42@users.noreply.github.com>
2024-07-25 16:52:22 +0530

    adding the repository component who contains the users all repositories information

--- Frontend/src/components/Repos.jsx
+++ Frontend/src/components/Repos.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Repo from './Repo'
 
 const Repos = () => {
   return (
-    <div>Repos</div>
+    <div className='lg:w-2/3 w-full bg-glassMorphismEffect rounded-lg px-8 py-6'>
+      <ol className='relative border-s bordergr200 '></ol>
+        <Repo/>
+        <Repo/>
+        <Repo/>
+        <Repo/>
+        <Repo/>
+    </div>
   )
 }
 
-export default Repos
+export default Repos;
